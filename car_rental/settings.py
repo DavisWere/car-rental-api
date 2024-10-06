911faccb8bf82b631b08857949e6deba7b428288
DavisWere <devisodhis10@gmail.com>
2024-10-06 20:41:32 +0300

    "Updated CORS settings: set CORS_ALLOW_ALL_ORIGINS to True and commented out CORS_ALLOWED_ORIGINS list"

--- car_rental/settings.py
+++ car_rental/settings.py
@@ -145,15 +145,15 @@ CSP_FRAME_ANCESTORS = ("'self'", 'https://smartinvoice.co.ke',
                         'https://car-rental-api-production-2584.up.railway.app')
 
 CORS_ALLOW_ALL_HEADERS = True
-CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = [
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
     
-    'http://localhost:3000',
-    'http://localhost:5173',
-    'http://localhost:5174',
-    'https://car-rental-api-production-2584.up.railway.app',
-    'http://car-rental-api-production-2584.up.railway.app',
-]
+#     'http://localhost:3000',
+#     'http://localhost:5173',
+#     'http://localhost:5174',
+#     'https://car-rental-api-production-2584.up.railway.app',
+#     'http://car-rental-api-production-2584.up.railway.app',
+# ]
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = [
