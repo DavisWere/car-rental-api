1966cf93d383c55ff6ee6f396c30809e8e4ce785
DavisWere <devisodhis10@gmail.com>
2024-10-06 19:53:25 +0300

    "Updated car_rental/urls.py to replace drf_yasg with drf_spectacular for API documentation"

--- car_rental/urls.py
+++ car_rental/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from rest_framework import permissions
 from django.conf import settings
 from django.conf.urls.static import static
@@ -40,10 +41,18 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    # Swagger URLs
-    path('swagger<str:format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # Swagger 
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'),
+         name='swagger-ui'),
+    path('api/schema/redoc/',
+         SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
+    # path('swagger<str:format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
     # Admin and other URLs
     path('admin/', admin.site.urls),
