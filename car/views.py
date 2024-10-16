6bc90fd1225b1cf1c3fa9b001f663507e36baa3b
DavisWere <devisodhis10@gmail.com>
2024-10-16 15:04:56 +0300

    "Updated CarViewSet permissions to allow unauthenticated access for list and retrieve actions"

--- car/views.py
+++ car/views.py
@@ -5,4 +5,11 @@ from car.serializers import CarSerializer
 class CarViewSet(viewsets.ModelViewSet):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
-    permission_classes = [permissions.IsAuthenticated]
+
+    def get_permissions(self):
+        # Allow viewing (retrieving) cars for anyone, including unauthenticated users
+        if self.action in ['list', 'retrieve']:
+            return [permissions.AllowAny()]
+        
+        # Require authentication for other actions like creating, updating, and deleting cars
+        return [permissions.IsAuthenticated()]
