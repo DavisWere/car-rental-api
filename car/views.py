d2279b272ed22a82d1fb9336688b88505df4f749
DavisWere <devisodhis10@gmail.com>
2024-10-16 15:23:06 +0300

    "Updated CarViewSet permissions and modified static file settings in settings.py"

--- car/views.py
+++ car/views.py
@@ -7,9 +7,7 @@ class CarViewSet(viewsets.ModelViewSet):
     serializer_class = CarSerializer
 
     def get_permissions(self):
-        # Allow viewing (retrieving) cars for anyone, including unauthenticated users
         if self.action in ['list', 'retrieve']:
             return [permissions.AllowAny()]
-        
-        # Require authentication for other actions like creating, updating, and deleting cars
+    
         return [permissions.IsAuthenticated()]
