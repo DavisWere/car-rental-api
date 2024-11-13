780a66daff4b7066a69199013208ce43710f1f1d
DavisWere <devisodhis10@gmail.com>
2024-11-13 11:35:02 +0300

    "Removed admin registration for CarMaintenance and Location models"

--- car_maintenance/admin.py
+++ car_maintenance/admin.py
@@ -1,5 +1,2 @@
 from django.contrib import admin
-from car_maintenance.models import CarMaintenance
-
-admin.site.register(CarMaintenance)
 
--- location/admin.py
+++ location/admin.py
@@ -1,4 +1,1 @@
-from django.contrib import admin
-from location.models import Location
 
-admin.site.register(Location)
