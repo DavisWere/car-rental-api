e914b87d653813e6d89077deb89a13bc0348eab2
DavisWere <devisodhis10@gmail.com>
2024-11-22 22:23:28 +0300

    "Added filtering to CarViewSet based on logged-in user and updated permissions"

--- car/views.py
+++ car/views.py
@@ -3,12 +3,28 @@ from car.models import Car
 from car.serializers import CarSerializer
 
 class CarViewSet(viewsets.ModelViewSet):
-    queryset = Car.objects.all()
+    """
+    ViewSet to manage Car objects with filtering based on car_owner.
+    """
     serializer_class = CarSerializer
 
+    def get_queryset(self):
+        """
+        Filters cars based on the logged-in user.
+        """
+        user = self.request.user
+        if self.action in ['list', 'retrieve']:  
+            return Car.objects.all()
+        if user.is_authenticated:
+            return Car.objects.filter(car_owner=user)
+        return Car.objects.none()  
 
     def get_permissions(self):
+        """
+        Assign permissions based on the action.
+        """
         if self.action in ['list', 'retrieve']:
-            return [permissions.AllowAny()]
-    
-        return [permissions.IsAuthenticated()]
+            return [permissions.AllowAny()]  
+        return [permissions.IsAuthenticated()]  
+
+
