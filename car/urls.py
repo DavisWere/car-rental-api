8beefc36c611127b7677cb3245b441bb178d6505
DavisWere <devisodhis10@gmail.com>
2024-11-23 19:18:12 +0300

    "Added PDFView to generate car list report and updated car/urls.py to include a new URL pattern for downloading the report"

--- car/urls.py
+++ car/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from car.views import CarViewSet
+from car.views import CarViewSet, PDFView
 
 car_router = DefaultRouter()
 car_router.register(r"cars",CarViewSet)
 
 urlpatterns = [
-    
+    path('cars/download/report/', PDFView.as_view(), name='car-list-pdf'),
 ]
 urlpatterns += car_router.urls
--- car/views.py
+++ car/views.py
@@ -1,3 +1,8 @@
+from rest_framework.views import APIView
+from xhtml2pdf import pisa
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from io import BytesIO
 from rest_framework import viewsets, permissions
 from car.models import Car
 from car.serializers import CarSerializer
@@ -29,3 +34,26 @@ class CarViewSet(viewsets.ModelViewSet):
         return [permissions.IsAuthenticated()]  
 
 
+
+class PDFView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+
+        cars = Car.objects.all() if user.is_superuser else  Car.objects.filter(car_owner=user)
+
+        serializer = CarSerializer(
+            cars, many=True, context={'request': request})
+        context = {'cars': serializer.data}
+
+        html_string = render_to_string('car_list.html', context)
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename=cars_list.pdf'
+
+        result = BytesIO()
+        pdf = pisa.pisaDocument(BytesIO(html_string.encode("UTF-8")), result)
+        if not pdf.err:
+            response.write(result.getvalue())
+            return response
+        return HttpResponse('We had some errors <pre>' + html_string + '</pre>')
