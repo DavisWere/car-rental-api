f64677c16a50afdedcb6c4250d59b9a1d19c55dc
DavisWere <devisodhis10@gmail.com>
2024-11-29 15:05:58 +0300

    "Update PAGE_SIZE in REST_FRAMEWORK settings"

--- car_rental/settings.py
+++ car_rental/settings.py
@@ -122,7 +122,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100increase page size fr,
+    'PAGE_SIZE': 100,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     "EXCEPTION_HANDLER": "drf_standardized_errors.handler.exception_handler"
 
